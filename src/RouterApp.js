1e1125c0dd4b330b6f498ed9824464e48508c9c8
Boscobrand <Tony.Bosco@boscobrand.com>
2022-05-24 09:44:16 -0400

    okta development

---
+++ src/RouterApp.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, withRouter } from 'react-router-dom';
+import { OktaAuth, toRelativeUrl } from '@okta/okta-auth-js';
+import { LoginCallback, Security, SecureRoute } from '@okta/okta-react';
+import Home from './Home';
+
+const oktaAuth = new OktaAuth({
+  issuer: 'https://dev-61688829.okta.com/oauth2/default',
+  clientId: '0oa55xgso1NUsEvgS5d7',
+  redirectUri: window.location.origin + '/login/callback'
+});
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.restoreOriginalUri = async (_oktaAuth, originalUri) => {
+      props.history.replace(toRelativeUrl(originalUri || '/', window.location.origin));
+    };
+  }
+
+  render() {
+    return (
+      <Security oktaAuth={oktaAuth} restoreOriginalUri={this.restoreOriginalUri}>
+        <Route path="/" exact={true} component={Home}/>
+        <Route path="/login/callback" component={LoginCallback}/>
+      </Security>
+    );
+  }
+}
+
+const AppWithRouterAccess = withRouter(App);
+
+class RouterApp extends Component {
+  render() {
+    return (<Router><AppWithRouterAccess/></Router>);
+  }
+}
+
+export default RouterApp;
